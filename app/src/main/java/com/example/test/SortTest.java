0fbd1a031c3a9e452215d382e14bff9f52d11e93
huangyonghuang <huangyonghuang@hualala.com>
2023-01-30 17:52:26 +0800

    快速排序test

--- app/src/main/java/com/example/test/SortTest.java
+++ app/src/main/java/com/example/test/SortTest.java
@@ -1,30 +1,25 @@
-package com.example.algorithm;
+package com.example.test;
 
-
-public class SortAlgorithm {
-
+/**
+ * @description: 快速排序、冒泡排序、选择排序
+ * @author: huangyonghuang
+ * @date: 2023/1/30
+ */
+public class SortTest {
 
     public static void main(String[] args) {
-        int[] array = new int[]{8, 9, 5, 6, 0, 1, 3, 6, 2, 4, 7};
-
-        //快速排序
-        quickSort(array, 0, array.length - 1);
-
-        //冒泡排序
-//        bubbleSort(array);
-
-        //选择排序
-//        selectSort(array);
-
-        for (int j : array) {
-            System.out.println(j);
-        }
+        int[] array = new int[]{1, 2, 9, 3, 5, 3, 8, 7};
+        int low = 0;
+        int high = array.length - 1;
+        quickSort(array, low, high);
+        bubbleSort(array);
+        selectSort(array);
     }
 
 
     /**
-     * 最好附上链接，以便后期更好理解该算法的设计思想
-     * <a href="https://www.cnblogs.com/captainad/archive/2019/06/10/10999697.html">...</a>
+     * 最好时间复杂度O(n),平均时间复杂度O(nlogn)
+     * https://www.cnblogs.com/captainad/archive/2019/06/10/10999697.html
      */
     private static void quickSort(int[] array, int low, int high) {
         if (low >= high) {
@@ -34,27 +29,21 @@ public class SortAlgorithm {
         i = low;
         j = high;
         temp = array[low];
-
         while (i < j) {
             while (temp <= array[j] && i < j) {
                 j--;
             }
-
             while (temp >= array[i] && i < j) {
                 i++;
             }
-
             if (i < j) {
-                t = array[j];
-                array[j] = array[i];
-                array[i] = t;
-
+                t = array[i];
+                array[i] = array[j];
+                array[j] = t;
             }
         }
-
         array[low] = array[j];
         array[j] = temp;
-
         //分别递归左右列表
         quickSort(array, low, j - 1);
         quickSort(array, j + 1, high);
@@ -62,13 +51,13 @@ public class SortAlgorithm {
 
 
     /**
-     * 冒泡排序,相邻的数字相互比较，大的数往后面移动
-     * <a href="https://blog.csdn.net/NathanniuBee/article/details/83413879">...</a>
+     * 最好时间复杂度O(n),平均时间复杂度O(n^2)
+     * https://blog.csdn.net/NathanniuBee/article/details/83413879
      */
     private static void bubbleSort(int[] array) {
         int temp;
         for (int j = 0; j < array.length - 1; j++) {
-            for (int i = 0; i < array.length - 1 - j; i++) {
+            for (int i = 0; i < array.length - j - 1; i++) {
                 if (array[i] > array[i + 1]) {
                     temp = array[i];
                     array[i] = array[i + 1];
@@ -80,26 +69,22 @@ public class SortAlgorithm {
 
 
     /**
-     * 选择排序
+     *
      * https://zhuanlan.zhihu.com/p/123048793
      */
     private static void selectSort(int[] array) {
         int temp;
         int minIndex;
-
         for (int j = 0; j < array.length - 1; j++) {
             minIndex = j;
-
             for (int i = minIndex; i < array.length - 1; i++) {
                 if (array[i + 1] < array[minIndex]) {
                     minIndex = i + 1;
                 }
             }
-
             temp = array[minIndex];
             array[minIndex] = array[j];
             array[j] = temp;
         }
     }
-
 }
