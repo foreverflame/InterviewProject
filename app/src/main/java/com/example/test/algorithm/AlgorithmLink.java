865c57c9870414d9fa2fa46db2f415a319e53449
huangyonghuang <huangyonghuang@hualala.com>
2023-02-10 10:13:07 +0800

    两个栈实现队列

---
+++ app/src/main/java/com/example/test/algorithm/AlgorithmLink.java
@@ -0,0 +1,14 @@
+package com.example.test.algorithm;
+
+/**
+ * @description:
+ * @author: huangyonghuang
+ * @date: 2023/2/10
+ */
+public class AlgorithmLink {
+
+    /**
+     * https://juejin.cn/post/6844903504331587597
+     */
+
+}
--- app/src/main/java/com/example/test/algorithm/NodeTest.java
+++ app/src/main/java/com/example/test/algorithm/NodeTest.java
@@ -2,6 +2,7 @@ package com.example.test.algorithm;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @description: 链表相关操作
@@ -150,6 +151,26 @@ public class NodeTest {
             E data = head.value;
             head = head.next;
             return data;
+        }
+    }
+
+    /**
+     * 用两个栈实现队列
+     */
+    public class MyQueue1 {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+
+        public void push(Integer data) {
+            stack1.push(data);
+        }
+        public Integer pop() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
         }
     }
 }
