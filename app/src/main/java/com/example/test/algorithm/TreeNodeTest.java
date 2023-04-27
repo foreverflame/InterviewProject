08d7aad6d1113af695ceaae944490de15c965fcd
huangyonghuang <huangyonghuang@hualala.com>
2023-04-27 19:50:32 +0800

    二叉树对称

--- app/src/main/java/com/example/test/algorithm/TreeNodeTest.java
+++ app/src/main/java/com/example/test/algorithm/TreeNodeTest.java
@@ -127,8 +127,27 @@ public class TreeNodeTest {
         if (node.data <= min || node.data >= max) {
             return false;
         }
-        //左子树
         return isValidBST(node.left, min, node.data) && isValidBST(node.right, node.data, max);
+    }
+
+
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        //从两个子节点开始判断
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        //如果左右子节点都为空，说明当前节点是叶子节点，返回true
+        if (left == null && right == null)
+            return true;
+        //如果当前节点只有一个子节点或者有两个子节点，但两个子节点的值不相同，直接返回false
+        if (left == null || right == null || left.data != right.data)
+            return false;
+        //然后左子节点的左子节点和右子节点的右子节点比较，左子节点的右子节点和右子节点的左子节点比较
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
 
 
