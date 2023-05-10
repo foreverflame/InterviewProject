670b19a7b53d96ec846204a52b87c65d3a5dccb3
huangyonghuang <huangyonghuang@hualala.com>
2023-05-10 10:21:39 +0800

    合并两个有序数组

--- app/src/main/java/com/example/test/algorithm/ArrayTest.java
+++ app/src/main/java/com/example/test/algorithm/ArrayTest.java
@@ -282,4 +282,26 @@ public class ArrayTest {
         return index;
     }
 
+
+    /**
+     * 合并两个有序数组
+     * 从后往前排
+     */
+    public static void getIndex(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums1[j--];
+            }
+        }
+        while (j > 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+
 }
