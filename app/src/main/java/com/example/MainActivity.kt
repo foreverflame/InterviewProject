d24760cfc37b181c62d5fd4cfac9ec8d555fdd85
huangyonghuang <huangyonghuang@hualala.com>
2023-05-18 11:22:07 +0800

    编译问题

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
 import com.bumptech.glide.Glide
-import com.example.kotlin.TestFlowViewModel
 import com.example.kotlin.inflate
 import com.example.widget.databinding.ActivityMain2Binding
-import com.example.workmanager.UploadWorker
 
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
     private val activityMain2Binding: ActivityMain2Binding by inflate()
-    private val testFlowViewModel: TestFlowViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         //glide
         Glide.with(this).load("").into(activityMain2Binding.image)
-
-        //WorkManager
-        val request = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
-        WorkManager.getInstance(this).enqueue(request)
-
-
     }
 
 }
