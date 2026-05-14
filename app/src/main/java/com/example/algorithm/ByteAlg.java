c8a7a5314696bed32dc90845e5d56637da86ef8f
huangyonghuang <huangyonghuang@360.cn>
2026-05-14 09:30:20 +0800

    二分有序数组查找

--- app/src/main/java/com/example/algorithm/ByteAlg.java
+++ app/src/main/java/com/example/algorithm/ByteAlg.java
@@ -452,4 +452,43 @@ public class ByteAlg {
     }
 
 
+    // 时间复杂度 O(log n), 空间复杂度 O(1)
+    // [1,2,3,3,3,3,4,5,9]
+    public int[] searchRange2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int first = -1;
+        int last = -1;
+        // 找第一个等于target的位置
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                first = middle;
+                right = middle - 1; //重点
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // 最后一个等于target的位置
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                last = middle;
+                left = middle + 1; //重点
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return new int[]{first, last};
+    }
+
+
 }
